1b511a23d3729c95b65997ae2d0b304d0102c16d
Wiktor <kasztanators2701@gmail.com>
2024-07-22 14:24:37 +0200

    Specific CRUD operations

---
+++ src/main/java/com/culinary/userservice/ingredient/mapper/SpecificMapper.java
@@ -0,0 +1,27 @@
+package com.culinary.userservice.ingredient.mapper;
+
+import com.culinary.userservice.ingredient.dto.SpecificDTO;
+import com.culinary.userservice.ingredient.model.Ingredient;
+import com.culinary.userservice.ingredient.model.Specific;
+import com.culinary.userservice.user.model.User;
+
+public class SpecificMapper {
+
+    public static Specific toEntity(SpecificDTO specificDto, User user, Ingredient ingredient) {
+        Specific specific = new Specific();
+        specific.setSpecificId(specificDto.getSpecificId());
+        specific.setUser(user);
+        specific.setIngredient(ingredient);
+        specific.setLikes(specificDto.getLikes());
+        return specific;
+    }
+
+    public static SpecificDTO toDto(Specific specific) {
+        return new SpecificDTO(
+                specific.getSpecificId(),
+                specific.getUser().getId(),
+                specific.getIngredient().getIngredientId(),
+                specific.getLikes()
+        );
+    }
+}
---
+++ src/main/java/com/culinary/userservice/ingredient/repository/SpecificRepository.java
@@ -0,0 +1,7 @@
+package com.culinary.userservice.ingredient.repository;
+
+import com.culinary.userservice.ingredient.model.Specific;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecificRepository extends JpaRepository<Specific, Integer> {
+}
