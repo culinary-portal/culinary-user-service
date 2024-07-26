fc522b911195053aa37ca1789ad974d72c16a542
Wiktor Czetyrbok <kasztanators2701@gmail.com>
2024-07-26 13:56:32 +0200

    DietType CRUD operations

--- src/main/java/com/culinary/userservice/recipe/repository/DietTypeRepository.java
+++ src/main/java/com/culinary/userservice/recipe/repository/DietTypeRepository.java
@@ -3,5 +3,8 @@ package com.culinary.userservice.recipe.repository;
 import com.culinary.userservice.recipe.model.DietType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DietTypeRepository extends JpaRepository<DietType, Integer>{
+import java.util.Optional;
+
+public interface DietTypeRepository extends JpaRepository<DietType, Integer> {
+    Optional<DietType> findDietTypeByDietType(String dietType);
 }
