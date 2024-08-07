a07625a7482e46f37fe756e6963f67e3161bc842
Wiktor Czetyrbok <kasztanators2701@gmail.com>
2024-08-07 09:46:43 +0200

    admin functionality

---
+++ src/main/java/com/culinary/userservice/user/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.culinary.userservice.user.controller;
+
+import com.culinary.userservice.user.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/api/admin")
+public class AdminController {
+    private final AdminService adminService;
+
+    @PutMapping("{userId}/block")
+    public ResponseEntity<Void> blockUser(@PathVariable long userId) {
+        adminService.blockUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("{userId}/unblock")
+    public ResponseEntity<Void> unblockUser(@PathVariable long userId) {
+        adminService.unblockUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{userId}/delete")
+    public ResponseEntity<Void> deleteUser(@PathVariable long userId) {
+        adminService.deleteUser(userId);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/culinary/userservice/user/service/AdminService.java
@@ -0,0 +1,29 @@
+package com.culinary.userservice.user.service;
+
+import com.culinary.userservice.user.model.User;
+import com.culinary.userservice.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    public void blockUser(long id) {
+        User user = userService.getUserEntityById(id);
+        user.setLocked(true);
+        userRepository.save(user);
+    }
+
+    public void unblockUser(long id) {
+        User user = userService.getUserEntityById(id);
+        user.setLocked(false);
+        userRepository.save(user);
+    }
+}
