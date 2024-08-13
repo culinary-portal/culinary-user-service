cc0fe449d74bb678b8416541396959db0842fc5a
Wiktor Czetyrbok <kasztanators2701@gmail.com>
2024-08-13 12:00:40 +0200

    add web config

---
+++ src/main/java/com/culinary/userservice/security/configuration/local/WebConfig.java
@@ -0,0 +1,29 @@
+package com.culinary.userservice.security.configuration.local;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Value("${frontend.host}")
+            private String host;
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(host)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
